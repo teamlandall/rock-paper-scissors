542b44806aaf52d3035ad11ca53c794d62a49a6e
landallproctor <landallproctor@gmail.com>
2017-03-27 09:16:01 -0400

    Mocks up play button and starting function.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,29 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      test: 'Ready'
+    }
+  }
+
+  playGame() {
+    setTimeout(() => {
+      this.setState({test: 'Rock'});
+    }, 1000);
+
+    setTimeout(() => {
+      this.setState({ test: 'Paper' });
+    }, 2000);
+
+    setTimeout(() => {
+      this.setState({ test: 'Scissor' });
+    }, 3000);
+  }
+
+
   render() {
     return (
       <div className="App">
@@ -11,8 +34,15 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          {this.state.test}
         </p>
+
+        <button 
+          id="play-button"
+          onClick={() => this.playGame()}
+        >
+          Play
+        </button>
       </div>
     );
   }
